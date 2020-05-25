d80355be1b5606097526211989edda26316d1380
ratanpratyush <ratan.pratyush@gmail.com>
2020-05-25 15:42:16 +0000

    check function updated

--- src/main.cc
+++ src/main.cc
@@ -1,29 +1,52 @@
 #include <releax>
+#include <releax-cc/net>
 
 using namespace std;
 
-int main_func(releax::cli& app)
-{
-    if (app.is_flagset("force")) {
+int check_func(releax::cli& app){
+    bool force = false;
 
+    string url = "https://releax.in";
+
+    string cus_url = app.val_of_arg("url");
+
+    if (len(cus_url) == 0)
+    cus_url = url;
+    else
+    url = cus_url;
+
+    releax::curl downloader;
+    string content;
+    string header;
+
+    downloader.url(url)
+              .content(content)
+              .header(header);
+
+    long resp = downloader.perform();
+
+    if (!downloader.good) {
+        cout << "error while fetching " << url << endl;
+        cout << "status code: " << resp << endl;
+        cout << "header: " << header << endl; 
+
+        return -2;
     }
 
-    if (app.is_flagset("r")){
+    cout << "header: " << header << endl;
+    cout << "Content: " << content << endl;
 
+    if (app.is_flagset("f") || app.is_flagset("force")) {
+        cout << "force flag set" << endl;
     }
 
     return 0;
-
 }
 
-int check_func(string url, string release){
-    cout << "Check function is working" << endl;
-
-}
-
-int update_func(int force_type, int backup_loc, int restore_point){
+int update_func(releax::cli& app){
     cout << "Update function is working" << endl;
 
+    return 0;
 }
 
 
@@ -36,9 +59,8 @@ int main(int ac, char** av)
        .release('a')
        .description("a new alternative sys-update")
        .author("Pratyush Ratan", "ratan.pratyush@gmail.com", "-")
-       .sub("update", "To update sys-update", "force-type", "backup-loc", "restore-point", update_func)
-       .sub("check", "To check something", "url", "release", check_func)
-       .main(main_func);
+       .sub("update", "To update sys-update", "force-type|backup-loc|restore-point", update_func)
+       .sub("check", "To check something", "url|release", check_func);
 
 
     return app.execute(ac, av);
